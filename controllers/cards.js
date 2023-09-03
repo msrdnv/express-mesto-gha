60e5df9a6835700f23494fd7c42fe18aadb03075
msrdnv <msrdnv@ya.ru>
2023-09-03 16:23:13 +0300

    feat: complete mesto api responses

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,8 +1,17 @@
 const Card = require('../models/card');
 
+const returnCardInfo = (data) => ({
+  likes: data.likes,
+  _id: data.id,
+  name: data.name,
+  link: data.link,
+  owner: data.owner,
+  createdAt: data.createdAt,
+});
+
 module.exports.findCards = (req, res) => {
   Card.find({})
-    .then((data) => res.send(data))
+    .then((data) => res.send(data.map((item) => returnCardInfo(item))))
     .catch((err) => console.log(err));
 };
 
@@ -10,16 +19,13 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
   Card.create({ name, link, owner })
-    .then((data) => res.send(data))
+    .then((data) => res.send(returnCardInfo(data)))
     .catch((err) => console.log(err));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((data) => {
-      res.send(data);
-      console.log('Карточка удалена');
-    })
+    .then(() => res.send({ message: 'Пост удалён' }))
     .catch((err) => console.log(err));
 };
 
@@ -29,7 +35,7 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true, runValidators: true },
   )
-    .then((data) => res.send(data))
+    .then((data) => res.send(returnCardInfo(data)))
     .catch((err) => console.log(err));
 };
 
@@ -39,6 +45,6 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true, runValidators: true },
   )
-    .then((data) => res.send(data))
+    .then((data) => res.send(returnCardInfo(data)))
     .catch((err) => console.log(err));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,21 +1,30 @@
 const User = require('../models/user');
 
+const returnUserInfo = (data) => ({
+  name: data.name, about: data.about, avatar: data.avatar, _id: data._id,
+});
+
 module.exports.findUsers = (req, res) => {
   User.find({})
-    .then((data) => res.send(data))
+    .then((data) => res.send(data.map((item) => returnUserInfo(item))))
     .catch((err) => console.log(err));
 };
 
 module.exports.findUser = (req, res) => {
   User.findById(req.params.userId)
-    .then((data) => res.send(data))
-    .catch((err) => console.log(err));
+    .then((data) => res.send(returnUserInfo(data)))
+    .catch((err) => {
+      if (err.name === 'CastError' || err.name === 'TypeError') {
+        return res.status(404).send({ message: 'Запрашиваемый пользователь не найден' });
+      }
+      return res.status(500).send({ message: 'На сервере произошла ошибка' });
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((data) => res.send(data))
+    .then((data) => res.send(returnUserInfo(data)))
     .catch((err) => console.log(err));
 };
 
@@ -23,7 +32,7 @@ module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
   const user = req.user._id;
   User.findByIdAndUpdate(user, { name, about }, { new: true, runValidators: true })
-    .then((data) => res.send(data))
+    .then((data) => res.send(returnUserInfo(data)))
     .catch((err) => console.log(err));
 };
 
@@ -31,6 +40,6 @@ module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
   const user = req.user._id;
   User.findByIdAndUpdate(user, { avatar }, { new: true, runValidators: true })
-    .then((data) => res.send(data))
+    .then((data) => res.send(returnUserInfo(data)))
     .catch((err) => console.log(err));
 };
