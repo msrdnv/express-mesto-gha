850a7a84d944b4b0b5d5ca67a2e978c660b49b4e
msrdnv <msrdnv@ya.ru>
2023-09-12 20:54:59 +0300

    fix: auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,13 +2,17 @@ const jwt = require('jsonwebtoken');
 const UnautorizedError = require('../utils/UnautorizedError');
 
 module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new UnautorizedError('Необходима авторизация'));
+  }
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(req.headers.authorization, 'e358ba70e8bff480040c82f19fec72ce1c27bb7a14a2a37002280a9b26559525');
+    payload = jwt.verify(token, 'e358ba70e8bff480040c82f19fec72ce1c27bb7a14a2a37002280a9b26559525');
   } catch (err) {
-    next(new UnautorizedError('Необходима авторизация'));
-    return;
+    return next(new UnautorizedError('Необходима авторизация'));
   }
   req.user = payload;
-  next();
+  return next();
 };
