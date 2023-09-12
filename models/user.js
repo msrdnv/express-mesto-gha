b311bae4a64162abfa1cea4eab6fed17016018e4
msrdnv <msrdnv@ya.ru>
2023-09-12 19:59:19 +0300

    fix: bug

--- models/user.js
+++ models/user.js
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUser = function findUser(email, password) {
+userSchema.statics.findUser = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
