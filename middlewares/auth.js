952746a4afd5d393358403fb3c471a383e4b0b37
msrdnv <msrdnv@ya.ru>
2023-09-12 20:02:53 +0300

    fix: delete env

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    payload = jwt.verify(req.headers.authorization, 'e358ba70e8bff480040c82f19fec72ce1c27bb7a14a2a37002280a9b26559525');
   } catch (err) {
     next(new UnautorizedError('Необходима авторизация'));
     return;
