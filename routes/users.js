083be666c21e51baf6564b1b11aa6d0706d1f81e
msrdnv <msrdnv@ya.ru>
2023-09-12 23:41:56 +0300

    fix: add regex

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
+const urlRegex = require('../utils/regex');
 const {
   findUsers, getCurrentUserInfo, findUser, updateProfile, updateAvatar,
 } = require('../controllers/users');
@@ -19,7 +20,7 @@ router.patch('/me', celebrate({
 }), updateProfile);
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+    avatar: Joi.string().pattern(urlRegex),
   }),
 }), updateAvatar);
 
